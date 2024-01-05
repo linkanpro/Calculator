cb41bae2068310eef18f4f3ae968174bf899ebb0
jenseneducation-linus-viklund <linus_timra@hotmail.com>
2024-01-05 10:17:48 +0100

    first commit

---
+++ Calculator/CalculatorTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Xunit.Sdk;
+
+public class CalculatorTest
+{
+    [Fact]
+    public void TestAddition()
+    {
+
+        // given
+        Calculator calc = new();
+        int a = 5;
+        int b = 2;
+        int expectedResult = 7;
+
+        // when
+        int result = calc.Add(a, b);
+
+        // then
+        Assert.Equal(expectedResult, result);
+    }
+}
